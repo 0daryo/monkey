cd07a0982d5913a8a88f8e97da16585ed5c7895f
ryotaro oda <plm23.ccc@gmail.com>
2019-10-29 23:43:23 +0900

    feat: parser

---
+++ parser/parser.go
@@ -0,0 +1,27 @@
+package parser
+
+import (
+	"github.com/0daryo/monkey/lexer"
+	"github.com/0daryo/monkey/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{
+		l: l,
+	}
+	p.nextToken()
+	p.nextToken()
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
