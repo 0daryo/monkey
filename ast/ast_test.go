8933583738396684c3e172e1e87a693e6b2d6cf5
ryotaro oda <plm23.ccc@gmail.com>
2019-11-09 12:56:39 +0900

    fix: ci points

--- ast/ast_test.go
+++ ast/ast_test.go
@@ -23,7 +23,7 @@ func TestString(t *testing.T) {
 				},
 				Value: &Identifier{
 					Token: token.Token{
-						Type: token.IDENT,
+						Type:    token.IDENT,
 						Literal: "anotherVar",
 					},
 					Value: "anotherVar",
